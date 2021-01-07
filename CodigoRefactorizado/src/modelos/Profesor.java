449380e36f70b5a9f12f65abf5bb42ed8f15cd75
Jose-Alcivar-C <josealci@espol.edu.ec>
2021-01-07 16:51:35 -0500

    Data Class

--- CodigoRefactorizado/src/modelos/Profesor.java
+++ CodigoRefactorizado/src/modelos/Profesor.java
@@ -2,29 +2,136 @@ package modelos;
 
 import java.util.ArrayList;
 
-public class Profesor {
-    public String codigo;
-    public String nombre;
-    public String apellido;
-    public int edad;
-    public String direccion;
-    public String telefono;
-    public InformacionAdicionalProfesor info;
-    public ArrayList<Paralelo> paralelos;
+public class Profesor 
+{
+    private int añosdeTrabajo;
+    private String facultad;
+    private double BonoFijo;
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String direccion;
+    private String telefono;
+    private ArrayList<Paralelo> paralelos;
 
-    public Profesor(String codigo, String nombre, String apellido, String facultad, int edad, String direccion, String telefono) {
+    public Profesor(int añosdeTrabajo, String codigo, String nombre, String apellido, String facultad, int edad, String direccion, 
+            String telefono) 
+    {
+        this.añosdeTrabajo = añosdeTrabajo;
         this.codigo = codigo;
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
         this.direccion = direccion;
         this.telefono = telefono;
-        paralelos= new ArrayList<>();
+        this.paralelos= new ArrayList<>();
     }
     
-    public void anadirParalelos(Paralelo p){
+    // Getters and Setters----------------------------------------------------
+    public void anadirParalelos(Paralelo p)
+    {
         paralelos.add(p);
     }
-    
-    
+
+    public int getAñosdeTrabajo() 
+    {
+        return añosdeTrabajo;
+    }
+
+    public void setAñosdeTrabajo(int añosdeTrabajo) 
+    {
+        this.añosdeTrabajo = añosdeTrabajo;
+    }
+
+    public String getFacultad() 
+    {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) 
+    {
+        this.facultad = facultad;
+    }
+
+    public double getBonoFijo() 
+    {
+        return BonoFijo;
+    }
+
+    public void setBonoFijo(double BonoFijo) 
+    {
+        this.BonoFijo = BonoFijo;
+    }
+
+    public String getCodigo() 
+    {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) 
+    {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() 
+    {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) 
+    {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() 
+    {
+        return edad;
+    }
+
+    public void setEdad(int edad) 
+    {
+        this.edad = edad;
+    }
+
+    public String getDireccion() 
+    {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) 
+    {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() 
+    {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) 
+    {
+        this.telefono = telefono;
+    }
+
+    public ArrayList<Paralelo> getParalelos() 
+    {
+        return paralelos;
+    }
+
+    public void setParalelos(ArrayList<Paralelo> paralelos) 
+    {
+        this.paralelos = paralelos;
+    }
 }
+
